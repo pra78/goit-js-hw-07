0707c56c2a47677fbec6199e6a1b1b3c89cb656c
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-10 14:23:28 +0200

    image list created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+
+const makeGalleryContainerMarkup = image => {
+  const { preview, original, description } = image;
+    return `
+        <li class="gallery-item">
+            <a href="${original}">
+                <img src="${preview}" alt="${description}" class="gallery-img">
+            </a>
+        </li>`;
+};
+
+const makeGalleryCard = galleryItems.map(makeGalleryContainerMarkup).join("");
+
+const makeGalleryList = `<ul>${makeGalleryCard}</ul>`;
+
+galleryContainer.insertAdjacentHTML("afterbegin", makeGalleryList);
+
 console.log(galleryItems);
