ba50e7981efa64af1d6daa99ec3aa7a47e2cb09b
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-14 23:04:47 +0200

    task 1 with onShow method

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,8 +5,6 @@ const galleryContainer = document.querySelector(".gallery");
 
 galleryContainer.addEventListener('click', onClick);
 
-let currentBasicLightbox = null;
-
 const makeGalleryContainerMarkup = galleryItems.map((image) => {
     const { preview, original, description } = image;
     return `
@@ -29,16 +27,16 @@ function onClick(event) {
         return;
     }
     event.preventDefault();
-    currentBasicLightbox = basicLightbox.create(`
-		<img width="1400" height="900" src="${event.target.dataset.source}">
-	`)
-    currentBasicLightbox.show();
-    document.addEventListener('keydown', onEscPressed);
-}
 
-function onEscPressed(event) {
-    if (event.code === "Escape") {
-        currentBasicLightbox.close();
-        document.removeEventListener("keydown", onEscPressed);
-    }
+    const html = `<img width="1400" height="900" src="${event.target.dataset.source}">`
+    
+    const currentBasicLightbox = basicLightbox.create(html, {
+        onShow: (basicLightbox) => {
+	        document.onkeydown = function(event) {
+		        if (event.code === "Escape") {
+			        basicLightbox.close();
+		        }
+	        };
+        },
+    }).show()
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -30,3 +30,18 @@ function onClick(event) {
 }
 
 console.log(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+
+// with jQuery nearly the same
+let gallery = $('.gallery a').simpleLightbox();
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
